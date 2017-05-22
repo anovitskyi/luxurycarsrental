c27d1f88c0187fa9b5f64c6b1ad11a5e6808935c
Andrii Novitskyi <novitskiyandrew@gmail.com>
2017-05-23 00:40:37 +0200

    Last commit

--- src/main/java/controllers/RentController.java
+++ src/main/java/controllers/RentController.java
@@ -4,11 +4,10 @@ import model.Car;
 import model.Client;
 import model.Rent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import service.CommonService;
 
 import javax.validation.Valid;
@@ -28,33 +27,95 @@ public class RentController
 
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public List<Rent> getAll()
+    public ResponseEntity<List<Rent>> getAll()
     {
-        return service.getAll();
+        List<Rent> list = service.getAll();
+        if (list.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        else
+            return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public Rent getById(@PathVariable int id)
+    public ResponseEntity<Rent> getById(@PathVariable int id)
     {
-        return service.get(id);
+        Rent rent = service.get(id);
+        if (rent == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else
+            return new ResponseEntity<>(rent, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public void add(@Valid Client client, BindingResult result, @Valid Car car, BindingResult result1, Date returningDate)
+    public ResponseEntity<Void> add(@RequestBody @Valid Client client, BindingResult result, @RequestBody @Valid Car car, BindingResult result1, Date returningDate)
     {
-        if (!result.hasErrors() && !result1.hasErrors())
+        if (result.hasErrors() || result1.hasErrors() || returningDate == null || returningDate.before(new Date()))
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        else
         {
+            // Set car unable
             car.setEnabled(false);
             car.setReturningDate(returningDate);
             serviceCar.update(car);
 
+            // Client rented car
             client.setCar(car);
             serviceClient.update(client);
 
+            // Saving
             service.add(new Rent(client, car, new Date()));
+
+            return new ResponseEntity<>(HttpStatus.OK);
         }
     }
 
-   /* @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public void update()*/
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@PathVariable int id, @RequestBody @Valid Client client, BindingResult result, @RequestBody @Valid Car car, BindingResult result1, Date returningDate)
+    {
+        Rent r = service.get(id);
+        if (r == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else if (result.hasErrors() || result1.hasErrors() || returningDate == null || returningDate.before(new Date()))
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        else
+        {
+            r.setCar(car);
+            r.setClient(client);
+            r.setDate(new Date());
+
+            // Set car unable
+            car.setEnabled(false);
+            car.setReturningDate(returningDate);
+            serviceCar.update(car);
+
+            // Client rented car
+            client.setCar(car);
+            serviceClient.update(client);
+
+            service.update(r);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable int id)
+    {
+        Rent rent = service.get(id);
+        if (rent == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else
+        {
+            Client client = rent.getClient();
+            client.setCar(null);
+            serviceClient.update(client);
+
+            Car car = rent.getCar();
+            car.setEnabled(true);
+            car.setReturningDate(null);
+            serviceCar.update(car);
+
+            service.delete(rent);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+    }
 }
